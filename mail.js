5a8e9a187b33eff43b534b0334ff0a910336de0b
stipica19 <34220970+stipica19@users.noreply.github.com>
2019-05-02 22:20:58 +0200

    Add files via upload

---
+++ mail.js
@@ -0,0 +1,32 @@
+const nodemailer = require('nodemailer');
+const mailGun = require('nodemailer-mailgun-transport');
+
+
+const auth = {
+    auth: {
+        api_key: 'xxx',
+        domain: 'xxx'
+    }
+};
+
+const transporter = nodemailer.createTransport(mailGun(auth));
+
+
+const sendMail = (email, subject, text, cb) => {
+    const mailOptions = {
+        from: email,
+        to: 'xxxx@gmail.com',
+        subject,
+        text
+    };
+
+    transporter.sendMail(mailOptions, function (err, data) {
+        if (err) {
+            cb(err, null);
+        } else {
+            cb(null, data);
+        }
+    });
+}
+
+module.exports = sendMail;
